a44d3f347f2d8ad521bc108528fdd60de3b5c719
Matt Hinchliffe <matt@maketea.co.uk>
2018-02-07 09:00:08 +0000

    Append validator methods to Rule prototype, not in constructor

--- src/core/rule.ts
+++ src/core/rule.ts
@@ -79,28 +79,10 @@ export class Rule {
   public validators: Criteria = []
   public sanitizers: Criteria = []
 
-  constructor () {
-    Object.entries(validator).forEach(([ name, method ]) => {
-      if (typeof method === 'function') {
-        this[name] = (...args): this => {
-          if (VALIDATORS.has(name)) {
-            this.validators.push({ method, args })
-          }
-
-          if (SANITIZERS.has(name)) {
-            this.sanitizers.push({ method, args })
-          }
-
-          return this
-        }
-      }
-    })
-  }
-
   static validate (instance: Rule, value: string): Boolean {
-    return instance.validators.every(({ method, args }) => {
-      return Reflect.apply(method, null, [ value, ...args ])
-    })
+    return instance.validators.every(({ method, args }) => (
+      Reflect.apply(method, null, [ value, ...args ])
+    ))
   }
 
   static sanitize (instance: Rule, value: string): any {
@@ -109,5 +91,19 @@ export class Rule {
     ), value)
   }
 }
+
+// Add every Validator method to the Rule prototype
+// NOTE: No Object.entries() in Node v6
+Object.keys(validator).forEach((name) => {
+  const method = validator[name]
+  const type = VALIDATORS.has(name) && 'validators' || SANITIZERS.has(name) && 'sanitizers'
+
+  if (type && typeof method === 'function') {
+    Rule.prototype[name] = function (...args) {
+      this[type].push({ method, args })
+      return this
+    }
+  }
+})
 
 export default (): Rule => new Rule()
