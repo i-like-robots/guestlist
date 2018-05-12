16d6b701b97a24267bd801d674b7b2190844ac71
Matt Hinchliffe <matt@maketea.co.uk>
2018-05-12 11:30:15 +0100

    Update example with latest syntax

--- examples/search-params.js
+++ examples/search-params.js
@@ -8,18 +8,20 @@ Try it out:-
   - http://localhost:3000?term=Hello+World&page=1
   - http://localhost:3000?term=With+a+date&date=2018-01-01
   - http://localhost:3000?term=With+an+invalid+page&page=101
+  - http://localhost:3000?tags=123&tags=456&tags=789
 
 Press ctrl+c to stop server.
 `
 
 const app = express()
 
-const query = guestlist.guard('query')
-  .permit('term', guestlist.rule().isLength({ min: 2 }).trim().escape())
-  .permit('page', guestlist.rule().isInt({ min: 1, max: 100 }).toInt())
-  .permit('date', guestlist.rule().isISO8601().toDate())
+const queryGuard = guestlist.guard()
+  .query('term', guestlist.rule().isLength({ min: 2 }).trim().escape())
+  .query('page', guestlist.rule().isInt({ min: 1, max: 100 }).toInt())
+  .query('date', guestlist.rule().isISO8601().toDate())
+  .query('tags', guestlist.rule().isInt().toInt(), { multiple: true })
 
-app.get('/', guestlist.secure(query), (req, res) => {
+app.get('/', queryGuard.secure(), (req, res) => {
   res.json(req.query)
 })
 
